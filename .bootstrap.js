6931aefe18c26f5818e9b1f10c6b46c12da6eede
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-14 15:56:31 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add i18next react-i18next i18next-browser-languagedetector"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
